cab1d260e10c8196309d54ab3ad679158f4f248c
Nathan <npitcha@sandia.gov>
2016-10-22 14:25:17 -0700

    need to add cell priority

--- sudoku.py
+++ sudoku.py
@@ -1,36 +1,44 @@
 from sys import argv
 
-class Grid:
-    def __init__(self):
-        self.grid = []
-        self.empty_spaces = list()
-        self.preset_chars = list()
-        row = 0
-        col = 0
-        with open(argv[1], 'r') as myFile:
-            for line in myFile:
+def define_grid():
+    grid = []
+    empty_spaces = list()
+    preset_chars = dict()
+    row = 0
+    col = 0
+    start = True
+    with open(argv[1], 'r') as myFile:
+        for line in myFile:
+            start2 = True
+            if start == True:
+                start = False
+            else:
                 row +=1
-                new_row = []
-                for char in line:
+            new_row = []
+            for char in line:
+                if start2 == True:
+                    start2 = False
+                else:
                     col +=1
-                    if char == '_':
-                        self.empty_spaces.append([row, col])
-                        new_row.append(None)
-                    elif char == '\r' or char == '\n':
-                        continue
-                    else:
-                        self.preset_chars.append(char)
-                        new_row.append(char)
-                self.grid.append(new_row)
+                if char == '_':
+                    empty_spaces.append([row, col])
+                    new_row.append('_')
+                elif char == '\r' or char == '\n':
+                    continue
+                else:
+                    preset_chars[(row,col)] = char
+                    new_row.append(char)
+            grid.append(new_row)
+    return grid, empty_spaces, preset_chars
 
 class state_node(object):
-    def __init__(self, parent=None, options=None, words=None, grid=None, variables=None, empty_spaces=None):
-        """parent -> the parent node, options -> the different options avalible at this step, variables-> the words left in the word bank"""
+    def __init__(self, parent=None, options=None, words=None, grid=None, set_char=None):
+        """parent -> the parent node, options -> the different options avalible at this step, words-> wordlist,  grid ->actual grid variables-> the words left in the word bank"""
         self.parent = parent
         self.options = options
         self.words = words
-        self.cur_grid = grid
-        self.variables = variables
+        self.grid = grid
+        self.set_char = set_char
 
 
 def word_list():
@@ -41,66 +49,80 @@ def word_list():
             for char in line:
                 if char == '\r' or char == '\n':
                     pass
-                        else:
-                            temp_line.append(char)
-                        ret_list.append(''.join(temp_line))
-return ret_list
+                else:
+                    temp_line.append(char)
+            temp = ''.join(temp_line)
+            ret_list.append(temp)
+        return ret_list
 
-def backtracking_search(state):
-    return recursive_backtracking(state)
 
 def valid_grid(state):
     """checks if we have a valid grid"""
     #checks rows
-    for line in state.cur_grid:
+    for line in state.grid:
         rowoccurence = [None, None, None, None, None, None, None, None, None, None]
         for char_index in range(0, len(line)):
             rowoccurence[char_index] = line[char_index]
         if  not len(rowoccurence) != len(set(rowoccurence)):
             return False
 
-#checks columns
-for i in range(0,9):
-    rowoccurence = [None, None, None, None, None, None, None, None, None, None]
+    #checks columns
+    for i in range(0,9):
+        rowoccurence = [None, None, None, None, None, None, None, None, None, None]
         for j in range(0, 9):
-            rowoccurence[i] = state.cur_grid[j][i]
-    if not len(rowoccurence) != len(set(rowoccurence)):
-        return False
+            rowoccurence[i] = state.grid[j][i]
+        if not len(rowoccurence) != len(set(rowoccurence)):
+            return False
 
 #check 3x3 squares
-check = []
+    check = []
     for j in range(3, 12, 3):
         check = []
         for i in range (0,3):
-            check.extend(state.cur_grid[i][:j])
+            check.extend(state.grid[i][:j])
         if not len(check) != len(set(check)):
             return False
         check = []
         for i in range(3, 6):
-            check.extend(state.cur_grid[i][:j])
+            check.extend(state.grid[i][:j])
         if not len(check) != len(set(check)):
             return False
         check = []
         for i in range(6, 9):
-            check.extend(state.cur_grid[i][:j])
+            check.extend(state.grid[i][:j])
         if not len(check) != len(set(check)):
             return False
 
-return True
+    return True
 
 def complete_grid(state):
     """check if the grid is completely filled not IF IT IS SOLVED!!!"""
-    for line in state.cur_grid:
+    for line in state.grid:
         if None in line:
             return False
     return True
 
+def backtracking_search(state):
+    return recursive_backtracking(state)
+
+def print_clean_grid(Grid):
+
+    for list in Grid:
+        my_string = []
+        for char in list:
+            if char == None:
+                my_string.append('_')
+            else:
+                my_string.append(char)
+        my_string.append('\n')
+        print ''.join(my_string)
+
 def recursive_backtracking(state):
-    if not valid_grid(state):
-        state = state.parent
-    #return invalidstate() --> I think you need to go back up the tree here
-    if complete_grid(state):
-        return state
+    # if not valid_grid(state):
+    #     state = state.parent
+    # #return invalidstate() --> I think you need to go back up the tree here
+    # if complete_grid(state):
+    #     return state
 
 # Find most constrained variables
 # Assign least constraining value to it (try vertically and then horizontally)
@@ -111,49 +133,68 @@ def recursive_backtracking(state):
 # recurse
 
     variable = (0, 0)
-    value = ''
-    
+    #print state.words
+    value = state.options[0]
+    word_done = False
+
     # vertical attempt
     vertical_succeeded = True
     for y in range(variable[1], variable[1] + len(value)):
-        if state.grid[variable[0], y] == 'None' or state.grid[variable[0], y] == value[y - variable[1]]:
-            state.grid[variable[0], y] = value[y - variable[1]]
+        #current grid element = _ or
+        if state.grid[variable[0]][y] == '_' or state.grid[variable[0]][y] == value[y - variable[1]]:
+            #print 'in herey'
+            #print "grid letter: " + state.grid[y][variable[1]] + " word letter: " + value[y - variable[0]]
+            state.grid[variable[0]][y] = value[y - variable[1]]
+            #print_clean_grid(state.grid)
+            #print 'if'
+            if y - variable[1] == len(value) - 1:
+                word_done = True
         else:
-            while state.grid[variable[0], variable[1]] != 'None':
-                state.grid[variable[0], y] = 'None'
+            while state.grid[variable[0]][variable[1]] != '_':
+                if not (variable[0],y) in state.set_char.keys():
+                    state.grid[variable[0]][y] = '_'
+                    #print_clean_grid(state.grid)
+                    #print 'else'
                 y -= 1
                 vertical_succeeded = False
-                break
+            break
     if vertical_succeeded:
         if valid_grid(state):
             # Remove variable and value from respective queues
-            recursive_backtracking(state)
+            if not word_done:
+                state.options.remove(value)
+                recursive_backtracking(state)
+            print_clean_grid(state.grid)
         else:
             return
 
     # horizontal attempt
     horizontal_succeeded = True
     for x in range(variable[0], variable[0] + len(value)):
-        if state.grid[x, variable[1]] == 'None' or state.grid[x, variable[1]] == value[x - variable[0]]:
-            state.grid[x, variable[1]] = value[x - variable[0]]
+        if state.grid[x][variable[1]] == '_' or state.grid[x][variable[1]] == value[x - variable[0]]:
+            #print 'in here2'
+            #print "grid letter: " + state.grid[x][variable[1]] + " word letter: " + value[x - variable[0]]
+            state.grid[x][variable[1]] = value[x - variable[0]]
+            if x - variable[1] == len(value) - 1:
+                word_done = True
         else:
-            while state.grid[variable[0], variable[1]] != 'None':
-                state.grid[x, variable[1]] = 'None'
-                y -= 1
+            #print 'in here'
+            while state.grid[variable[0]][variable[1]] != '_':
+                if not (variable[0], variable[1]) in state.set_char.keys():
+                    #print("letter deleted: " + state.grid[variable[0], variable[1]])
+                    state.grid[x][variable[1]] = '_'
+                x -= 1
                 horizontal_succeeded = False
-                break
-
+            break
     if horizontal_succeeded:
         if valid_grid(state):
             # Remove variable and value from respective queues
-            recursive_backtracking(state)
-        return
 
-
-
-
-
-
+            if not word_done:
+                state.options.remove(value)
+                recursive_backtracking(state)
+            print_clean_grid(state.grid)
+    return
 
 #make a priority queue ---> most contraint variables if tie then most constraining variable
 #for x in priority_queue
@@ -174,14 +215,16 @@ def recursive_backtracking(state):
     """
 def main():
     # make a grid object which contains space objects
-    myGrid = Grid()
+    myGrid, empty_chars, preset_chars = define_grid()
     myWordList = word_list()
-    myWordList.sort(lambda  x,y: cmp(len(x), len(y)))
-    print myWordList
-    if myGrid.preset_chars != None:
-        rootState = state_node(myGrid.preset_chars, myWordList, None, myGrid, myGrid.empty_spaces)
+    #myWordList.sort(lambda  x,y: cmp(len(x), len(y)))
+    myWordList= sorted(myWordList, key=lambda x: len(x))
+    if preset_chars != None:
+        rootState = state_node(None, myWordList, myWordList, myGrid, preset_chars)
     else:
-        rootState = state_node(None, myWordList, None, myGrid, myGrid.empty_spaces)
+        rootState = state_node(None, myWordList, myWordList, myGrid, preset_chars)
+    #"parent -> the parent node, options -> the different options avalible at this step,    words-> wordlist  grid ->actual grid variables-> the words left in the word bank"""
+    #print_clean_grid(myGrid)
     backtracking_search(rootState)
 
 
